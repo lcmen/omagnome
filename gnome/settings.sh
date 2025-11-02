d520dcdd4b278593107fc1507cafaa8ea7354a7f
Lucas Mendelowski <lucas@mendelowski.com>
2025-11-02 19:27:13 +0100

    Disable auto maximize

--- gnome/settings.sh
+++ gnome/settings.sh
@@ -2,16 +2,16 @@
 
 echo "Configuring GNOME settings..."
 
-# Disable mouse acceleration (use flat acceleration profile)
 echo "  - Disabling mouse acceleration..."
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 
-# Disable screen edge tiling
 echo "  - Disabling screen edge tiling..."
 gsettings set org.gnome.desktop.wm.preferences edge-tiling false
 
-# Disable hot corners
 echo "  - Disabling hot corners..."
 gsettings set org.gnome.desktop.interface enable-hot-corners false
+
+echo "  - Disabling auto-maximize (Forge will handle it)..."
+gsettings set org.gnome.mutter auto-maximize false
 
 echo "GNOME settings configured successfully!"
